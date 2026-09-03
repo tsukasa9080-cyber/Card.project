a3488116154f3da1565cb54ed0965ebe54cba506
Tsukasa9080-cyber <Tsukasa9080@gmail.com>
2026-09-03 23:41:36 +0900

    カードの更新

--- Card.project/Card_projectApp.swift
+++ Card.project/Card_projectApp.swift
@@ -1,28 +1,8 @@
-//
-//  Card_projectApp.swift
-//  Card.project
-//
-//  Created by G-2028 on 2026/08/18.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct Card_projectApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
